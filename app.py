1ea00c0bce3d94a6aa23c61b503e9e40a018b238
gogoandcindy <132862728+gogoandcindy@users.noreply.github.com>
2023-05-09 08:51:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,26 +11,28 @@ from linebot.models import *
 from selenium import webdriver
 
 # ======python的函數庫==========
+import requests
 import tempfile
 import os
 import datetime
 import openai
 import time
+from bs4 import BeautifulSoup
 # ======python的函數庫==========
 import configparser
-
+from requests_html import HTMLSession
 app = Flask(__name__)
 
-""" config = configparser.ConfigParser()
+config = configparser.ConfigParser()
 config.read('config.ini')
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret')) """
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+""" static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET')) """
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -54,21 +56,27 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
-    # 创建一个 Chrome 浏览器实例
-    driver = webdriver.Chrome()
+    # 创建一个 HTMLSession 实例
+    url = 'https://sys.leadyoung.com.tw/assets/Home/LINE_BOT_TEST?ID=2023/05/08'
 
-    # 打开网页
-    driver.get("https://sys.leadyoung.com.tw/assets/Home/LINE_BOT_TEST?ID="+msg)
+    # 發送GET請求
+    response = requests.get(url)
 
-    # 等待 JS 加载完成
-    driver.implicitly_wait(10)
+    # 解析HTML內容
+    soup = BeautifulSoup(response.text, 'html.parser')
+    time.sleep(5000)
+    # 找到所有的超連結
+    links = soup.find_all(
+        'div', class_="dd", limit=1)
 
-    # 获取网页内容
-    content = driver.page_source
+    # 輸出每個超連結的文字和URL
+    for link in links:
+        print(link.text)
 
-    # 关闭浏览器
-    driver.quit()
-    """ driver = webdriver.Chrome('./chromedriver.exe')
+
+# 等待 JS 加载完成
+""" response.html.render() """
+""" driver = webdriver.Chrome('./chromedriver.exe')
     driver.get(
         'https://sys.leadyoung.com.tw/assets/Home/LINE_BOT_TEST?ID='+msg)
     time.sleep(10) """
